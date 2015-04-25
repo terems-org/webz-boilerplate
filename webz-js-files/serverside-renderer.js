a0d26c2033422f52cb81229a14e1a03918b2f2c0
teremterem <toporok@gmail.com>
2015-04-25 23:57:07 +0300

    WebZ Boilerplate - initial commit

---
+++ webz-js-files/serverside-renderer.js
@@ -0,0 +1,31 @@
+
+var indexTemplate = null;
+
+var webzInit = function(webzFiles) {
+	indexTemplate = Handlebars.compile(webzFiles.getFile("/index.html").getFileDownloader().getContentAsStringAndClose());
+	indexTemplate({}); // for some reason this does some kind of template initialization (which otherwise happens upon first pageload)
+};
+
+var webzPreparePageContext = function(webzContext, fullUrl) {
+
+	var currentFile = webzContext.getCurrentFile();
+
+	var pageContext = webzContext.getFile(currentFile.getPathname()+".json").getFileDownloader();
+	if (pageContext != null) {
+		pageContext = JSON.parse(pageContext.getContentAsStringAndClose());
+	}
+	pageContext = pageContext||{};
+	var currentDownloader = currentFile.getFileDownloader();
+	if (currentDownloader != null) {
+		pageContext['WEBZ-MAIN-CONTENT'] = currentDownloader.getContentAsStringAndClose();
+	}
+	return pageContext;
+};
+
+var webzRenderPage = function(pageContext) {
+	var webzMainContent = pageContext['WEBZ-MAIN-CONTENT'];
+	if (webzMainContent) {		
+		pageContext['WEBZ-MAIN-CONTENT'] = marked(webzMainContent);
+	}
+	return indexTemplate(pageContext);
+};
