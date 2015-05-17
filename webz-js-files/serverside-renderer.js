788ad11aa55c2fd686c0684fc283c1a1e397ed6f
teremterem <toporok@gmail.com>
2015-05-17 08:26:22 +0300

    introduced jquery.extend.js

--- webz-js-files/serverside-renderer.js
+++ webz-js-files/serverside-renderer.js
@@ -1,27 +1,36 @@
 
-var indexTemplate = null;
+var indexTemplate;
+var globalContext = {};
 
 var webzInit = function(webzFiles) {
 	indexTemplate = Handlebars.compile(webzFiles.getFile("/index.html").getFileDownloader().getContentAsStringAndClose());
 	Handlebars.registerPartial("index.hbs", webzFiles.getFile("/app/templates/index.hbs").getFileDownloader().getContentAsStringAndClose());
 	Handlebars.registerPartial("end-of-body.hbs", webzFiles.getFile("/app/templates/end-of-body.hbs").getFileDownloader().getContentAsStringAndClose());
 	indexTemplate({}); // for some reason this does some kind of template initialization (which otherwise happens upon first pageload)
+
+	var context = webzFiles.getFile("global.json").getFileDownloader();
+	if (context != null) {
+		globalContext = JSON.parse(context.getContentAsStringAndClose());
+	}
 };
 
 var webzPreparePageContext = function(webzContext, fullUrl) {
 
 	var currentFile = webzContext.getCurrentFile();
+	var pageContext = {};
 
-	var pageContext = webzContext.getFile(currentFile.getPathname()+".json").getFileDownloader();
-	if (pageContext != null) {
-		pageContext = JSON.parse(pageContext.getContentAsStringAndClose());
+	var context = webzContext.getFile(currentFile.getPathname()+".json").getFileDownloader();
+	if (context != null) {
+		pageContext = JSON.parse(context.getContentAsStringAndClose());
 	}
-	pageContext = pageContext||{};
+
+	var resultingContext = $.extend({}, globalContext, pageContext);
+
 	var currentDownloader = currentFile.getFileDownloader();
 	if (currentDownloader != null) {
-		pageContext['WEBZ-MAIN-CONTENT'] = currentDownloader.getContentAsStringAndClose();
+		resultingContext['WEBZ-MAIN-CONTENT'] = currentDownloader.getContentAsStringAndClose();
 	}
-	return pageContext;
+	return resultingContext;
 };
 
 var webzRenderPage = function(pageContext) {
